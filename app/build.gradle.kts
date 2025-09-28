66215cb356eb74f778bdd43c7c27d508366f2213
AKS Labs <adityashinde.info@gmail.com>
2025-09-28 23:59:51 +0530

    version update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,8 +43,8 @@ android {
         applicationId = "com.akslabs.SandeshVahak"
         minSdk = 29
         targetSdk = 36
-        versionCode = 3
-        versionName = "0.3"
+        versionCode = 2
+        versionName = "0.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
